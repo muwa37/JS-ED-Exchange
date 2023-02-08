2c395134e21ccdc78fa06a78c3b54062a57a491f
muwa37 <yond41m3@icloud.com>
2023-02-08 22:00:39 +0600

    Added logout

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,18 @@
 "use strict";
 
 const logoutButton = new LogoutButton();
+
+let callback = method => response => (response.success) && method(response.data);
+
+function updateTable(data) {
+    ratesBoard.clearTable();
+    ratesBoard.fillTable(data);
+}
+
+let updateStocks = () => ApiConnector.getStocks(callback(updateTable));
+
+logoutButton.action = () => ApiConnector.logout(callback(window.location.reload.bind(window.location)));
+ApiConnector.current(callback(ProfileWidget.showProfile));
+ApiConnector.getFavorites(callback(updateFavorites));
+updateStocks();
+setInterval(updateStocks, 60000);
